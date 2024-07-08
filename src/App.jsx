5088ec7e4231ed647f8867ca4fa437586ffc1fe8
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2024-07-08 19:27:18 +0300

    route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import AppRouter from './routing/Router.jsx';
+import AppRouter from './routing/Router';
+import Navbar from './components/navbar';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <AppRouter />
     </div>
   );
