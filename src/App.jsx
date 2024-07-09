ff544fedb9f176887dbf28c244731c64b8c6b202
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2024-07-09 12:28:34 +0300

    lazy loading added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MainLayout from "./layout/MainLayout";
+import MainLayout from "./layout/MainLayout.jsx";
 import { lazy } from "react";
-import { delayImport } from "./components/delayImport";
+import { delayImport } from "./components/delayImport.jsx";
 
 const Home = lazy(() => import("./pages/Home"));
 const Contact = lazy(() => delayImport(() => import("./pages/Contact"), 2000));
